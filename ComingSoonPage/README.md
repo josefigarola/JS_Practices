1288f7b68e341d75f70ecf145fcf789277b8ce5d
josefigarola <jmfigarolaprado2@gmail.com>
2023-10-09 13:15:22 -0600

    coming soon page

--- ComingSoonPage/README.md
+++ ComingSoonPage/README.md
@@ -1,5 +1,27 @@
-# <h1 align="center">Templates</h1>
+# <h1 align="center">Coming Soon Page App</h1>
 
-Both scripts are templates for the index HTML file and the plain CSS file.
-This templates were creared in order to be able to be used for other apps.
-The intention is to copy and paste templates and begin coding.
+A simple web application that displays a "Coming Soon" page with a countdown timer to the launch date.
+
+## Features
+
+- Visually appealing "Coming Soon" page.
+- Real-time countdown timer to the launch date.
+- "Learn More" button to provide additional information.
+
+## Usage
+
+1. Open the app in your web browser.
+
+2. You will see a "Coming Soon" page with a countdown timer.
+
+3. The countdown timer displays the remaining days, hours, minutes, and seconds until the launch date.
+
+4. Once the launch date arrives, the countdown timer will show "00" for all units.
+
+5. Click the "Learn More" button to get more details about the launch.
+
+## Demo
+
+<p align="center">
+  <img src="images/demo.png" alt="Demo Screenshot">
+</p>
--- ComingSoonPage/script.js
+++ ComingSoonPage/script.js
@@ -1,1 +1,27 @@
-console.log('Hello world!')
+// var countDownDate = new Date('Oct 30, 2023 00:00:00').getTime();
+var countDownDate = new Date('2023-10-30T00:00:00').getTime(); // ISO 8601 format
+
+var x = setInterval(function(){
+
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById('days').innerHTML = days;
+    document.getElementById('hours').innerHTML = hours;
+    document.getElementById('minutes').innerHTML = minutes;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    if(distance < 0){
+        clearInterval(x);
+        document.getElementById('days').innerHTML = '00';
+        document.getElementById('hours').innerHTML = '00';
+        document.getElementById('minutes').innerHTML = '00';
+        document.getElementById('seconds').innerHTML = '00';
+    }
+
+},1000);

c152c2ce9c5776f1049a9475d94286bc2dae99b7
josefigarola <jmfigarolaprado2@gmail.com>
2023-10-10 13:11:39 -0600

    circular progress abr

--- CircularProgressBar/script.js
+++ CircularProgressBar/script.js
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+let number = document.getElementById('number');
+let counter = 0;
+
+setInterval(() => {
+    if(counter == 100){
+        clearInterval();
+    }
+    else{
+        counter += 1;
+        number.innerHTML = counter + '%';
+    }
+},18);
